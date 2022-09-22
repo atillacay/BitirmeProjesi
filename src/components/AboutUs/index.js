e4a2860b3410facad57b77a442865b92f2219570
Alper Cay <ialpercay@gmail.com>
2022-09-22 17:23:36 +0300

    CssFix

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -7,15 +7,9 @@ import twitter from "../../5282551_tweet_twitter_twitter logo_icon.png";
 const AboutUs = () => {
   return (
     <div className="container">
-      <div className="row -mt-[75px] ">
-        <div className="col __lg:mt-[100px] __sm:mt-[200px] flex flex-wrap items-center">
-          <div className="col-lg-4 p-8 ">
-            <img
-              src="https://thumbs.dreamstime.com/b/conception-bleue-d-illustration-du-fond-de-l-ic%C3%B4ne-abr%C3%A9g%C3%A9-pages-document-isol%C3%A9-sur-la-abstraite-167325917.jpg"
-              alt=""
-            />
-          </div>
-          <div className="col-lg-8 p-8">
+      <div className="row -mt-[25px] __lg:mt-[100px] ">
+        <div className="  flex flex-wrap items-center">
+          <div className="col-lg-8 col-sm-8 p-8">
             <h2 className="text-2xl ">Our Mission</h2>
             <p className="text-sm mb-2">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
@@ -30,39 +24,50 @@ const AboutUs = () => {
               placeat, doloribus rerum eum earum voluptatibus optio maxime sint,
             </p>
           </div>
-        </div>
-        <div className="row">
-          <div className="col flex mb-5 ">
-            <div className="col-lg-8 p-8">
-              <h2 className="text-2xl ">Our Mission</h2>
-              <p className="text-sm mb-2">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
-                asperiores dolor sunt facere accusamus consequatur. Sequi
-                debitis iste cupiditate, exercitationem veritatis dolores
-                obcaecati dignissimos possimus temporibus. Dolores quos a
-                accusamus! Ad quia sed nisi hic, dolores explicabo consectetur
-                beatae ullam minus reiciendis ea deserunt error aperiam
-                aspernatur laboriosam adipisci? Quaerat iure inventore repellat
-                nam iusto accusamus officia eveniet nemo! Optio. Eos, quas
-                delectus. Ducimus dolores molestiae hic minus sed tempora enim
-                autem iste exercitationem placeat, doloribus rerum eum earum
-                voluptatibus optio maxime sint,
-              </p>
-            </div>
-
-            <div className="col-lg-4 flex justify-center p-2">
-              <img
-                src="https://thumbs.dreamstime.com/b/pdf-document-page-icon-isolated-abstract-blue-background-illustration-design-pdf-document-page-icon-abstract-blue-background-167325366.jpg"
-                alt=""
-              />
-            </div>
+          <div className="col-lg-4 col-sm-4 p-8 ">
+            <img
+              src="https://thumbs.dreamstime.com/b/conception-bleue-d-illustration-du-fond-de-l-ic%C3%B4ne-abr%C3%A9g%C3%A9-pages-document-isol%C3%A9-sur-la-abstraite-167325917.jpg"
+              alt=""
+            />
           </div>
         </div>
+        <div className="  flex flex-wrap items-center">
+          <div className="col-lg-4 col-sm-4  p-8 hidden sm:block md:block lg:block">
+            <img
+              className=""
+              src="https://thumbs.dreamstime.com/b/conception-bleue-d-illustration-du-fond-de-l-ic%C3%B4ne-abr%C3%A9g%C3%A9-pages-document-isol%C3%A9-sur-la-abstraite-167325917.jpg"
+              alt=""
+            />
+          </div>
+          <div className="col-lg-8 col-sm-8 p-8">
+            <h2 className="text-2xl ">Our Vision</h2>
+            <p className="text-sm mb-2">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
+              asperiores dolor sunt facere accusamus consequatur. Sequi debitis
+              iste cupiditate, exercitationem veritatis dolores obcaecati
+              dignissimos possimus temporibus. Dolores quos a accusamus! Ad quia
+              sed nisi hic, dolores explicabo consectetur beatae ullam minus
+              reiciendis ea deserunt error aperiam aspernatur laboriosam
+              adipisci? Quaerat iure inventore repellat nam iusto accusamus
+              officia eveniet nemo! Optio. Eos, quas delectus. Ducimus dolores
+              molestiae hic minus sed tempora enim autem iste exercitationem
+              placeat, doloribus rerum eum earum voluptatibus optio maxime sint,
+            </p>
+          </div>
+          <div className="col-lg-4 col-sm-4 p-8 block sm:hidden md:hidden lg:hidden">
+            <img
+              className=""
+              src="https://thumbs.dreamstime.com/b/conception-bleue-d-illustration-du-fond-de-l-ic%C3%B4ne-abr%C3%A9g%C3%A9-pages-document-isol%C3%A9-sur-la-abstraite-167325917.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+
         <div className="row flex  justify-center">
           <div className="col-12 p-2 flex  justify-center">
             <h1 className="text-lg">Follow Us On</h1>
           </div>
-          <div className="col-3 flex items-center  bg-white/70 rounded-lg">
+          <div className="col-12 flex items-center  bg-white/70 rounded-lg">
             <div className="col-3 flex  justify-center p-1">
               <img
                 className="w-25 h-25 cursor-pointer hover:scale-105"
